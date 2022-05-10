c28ae772c2622db4d1db13abfe1304e849348b06
risako070310 <risako070310@gmail.com>
2022-05-10 11:19:31 +0900

    add title row layout

--- lib/main.dart
+++ lib/main.dart
@@ -10,29 +10,53 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500]
+          ),
+          const Text('41')
+        ],
+      ),
+    );
     return MaterialApp(
       title: 'Flutter layout Demo',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout Demo'),
         ),
-        body: const Center(
-            child: Text('Hello World!!'),
+        body: Column(
+            children: [
+              titleSection,
+            ],
+          ),
         ),
-      ),
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.pink,
-      )
-    );
+      );
   }
 }
 
